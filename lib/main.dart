425944ba006f8df05775dae6ae66630be623e44f
KonstantinLeschenko <mypost.kh@gmail.com>
2025-07-30 21:00:35 +0300

    provider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:counter_app/providers/counter_provider.dart';
 import 'package:counter_app/view/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,6 +23,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.ribeyeMarrowTextTheme(),
+        snackBarTheme: SnackBarThemeData(backgroundColor: Colors.amber[900]),
       ),
       home: HomeScreen(),
     );
--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,15 +1,10 @@
+import 'package:counter_app/providers/counter_provider.dart';
 import 'package:counter_app/view/my_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -28,20 +23,66 @@ class _HomeScreenState extends State<HomeScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    _counter.toString(),
-                    style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                      color: Colors.white,
-                      fontSize: 300,
-                    ),
+                  Consumer<CounterProvider>(
+                    builder: (context, counterProvider, child) {
+                      return Text(
+                        counterProvider.count.toString(),
+                        style: Theme.of(context).textTheme.displayLarge!
+                            .copyWith(color: Colors.white, fontSize: 300),
+                      );
+                    },
                   ),
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  MyButton(title: '-', onPressed: _decrementCounter),
-                  MyButton(title: '+', onPressed: _incrementCounter),
+                  MyButton(
+                    title: '-',
+                    onPressed: () {
+                      final counterProvider = Provider.of<CounterProvider>(
+                        context,
+                        listen: false,
+                      );
+                      if (counterProvider.count > 0) {
+                        counterProvider.decrement();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              'El contador no puede bajar de 0',
+                              style: Theme.of(context).textTheme.displayLarge!
+                                  .copyWith(color: Colors.black, fontSize: 30),
+                            ),
+                            duration: Duration(seconds: 1),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  MyButton(
+                    title: '+',
+                    onPressed: () {
+                      final counterProvider = Provider.of<CounterProvider>(
+                        context,
+                        listen: false,
+                      );
+                      if (counterProvider.count < 10) {
+                        counterProvider.increment();
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              'El contador no puede subir de 10',
+                              style: Theme.of(context).textTheme.displayLarge!
+                                  .copyWith(color: Colors.black, fontSize: 30),
+                            ),
+                            duration: Duration(seconds: 1),
+                          ),
+                        );
+                      }
+                    },
+                  ),
                 ],
               ),
             ],
@@ -49,49 +90,5 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
-  }
-
-  void _incrementCounter() {
-    if (_counter < 10) {
-      setState(() {
-        _counter++;
-      });
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.amber[900],
-          content: Text(
-            'El contador no puede exceder 10',
-            style: Theme.of(context).textTheme.displayLarge!.copyWith(
-              color: Colors.black,
-              fontSize: 30,
-            ),
-          ),
-          duration: Duration(seconds: 1),
-        ),
-      );
-    }
-  }
-
-  void _decrementCounter() {
-    if (_counter > 0) {
-      setState(() {
-        _counter--;
-      });
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.amber[900],
-          content: Text(
-            'El contador no puede bajar de 0',
-            style: Theme.of(context).textTheme.displayLarge!.copyWith(
-              color: Colors.black,
-              fontSize: 30,
-            ),
-          ),
-          duration: Duration(seconds: 1),
-        ),
-      );
-    }
   }
 }
